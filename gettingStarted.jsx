4b80b38a1c50179365180354a782cb5a1083a8b0
kash <kashyap76315@outlook.com>
2023-10-02 12:43:45 -0400

    certain files Updated

--- gettingStarted.jsx
+++ gettingStarted.jsx
@@ -4,10 +4,16 @@ import './styles/main.css';
 
 import Header from './components/header/Header';
 import Example from './components/example/Example';
+import States from "./components/states/States";
 
 ReactDOM.render(
     <Header/>,
     document.getElementById('reactappHeader')
+);
+
+ReactDOM.render(
+    <States/>,
+    document.getElementById('reactappStates')
 );
 
 ReactDOM.render(
--- modelData/example.js
+++ modelData/example.js
@@ -16,6 +16,7 @@ if (models === undefined) {
 
 models.exampleModel = function () {
   return {
-    name: "Unknown Name",
+    name: "Ka$hApp",
+    motto: "We pop out at your party..",
   };
 };
