d9b3ce47ebf466eb5e7aa944c4487e80a562bc23
kash <kashyap76315@outlook.com>
2023-10-02 16:39:25 -0400

    everything

---
+++ modelData/example.js
@@ -0,0 +1,20 @@
+/*
+ * Load the model data for Project 4, Problem 1. We load into DOM the
+ * property models.exampleModel a function that returns an object with the
+ * following property:
+ *    name:  A string with name.
+ *
+ * See README.md for information on how to access it.
+ */
+var models;
+
+if (models === undefined) {
+  models = {};
+}
+
+models.exampleModel = function () {
+  return {
+    name: "Ka$hApp",
+    motto: "We pop out at your party..."
+  };
+};
