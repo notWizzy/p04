85158681d3ae711019f7ad4ecdddd2840a50f79f
Dmool <143208768+dmool@users.noreply.github.com>
2023-10-02 01:16:48 -0400

    Wrote the header component

---
+++ components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div>
+      <header className="header">
+          <h1 className="title">WELCOME TO OUR PAGE</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
