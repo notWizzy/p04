5b62dd8e47816164be79c4fb6b3edbac65d8af08
kash <kashyap76315@outlook.com>
2023-10-02 12:32:23 -0400

    getting-started.jsx Updated

--- gettingStarted.jsx
+++ gettingStarted.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.css';
 
+import Header from './components/header/Header';
 import Example from './components/example/Example';
 
 ReactDOM.render(
-  <Example />,
-  document.getElementById('reactapp'),
+    <Header/>,
+    document.getElementById('reactappHeader')
+);
+
+ReactDOM.render(
+    <Example />,
+    document.getElementById('reactapp')
 );
