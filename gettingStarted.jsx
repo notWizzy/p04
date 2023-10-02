7fce1e406562ff7a3329e303e7d252002d541bf1
Kashyap Patel <106133901+notWizzy@users.noreply.github.com>
2023-10-02 16:32:07 -0400

    Add files via upload

---
+++ gettingStarted.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/main.css';
+
+import Header from './components/header/Header';
+import Example from './components/example/Example';
+
+ReactDOM.render(
+    <Header/>,
+    document.getElementById('reactappHeader')
+);
+
+ReactDOM.render(
+    <Example />,
+    document.getElementById('reactapp')
+);
---
+++ p2.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import States from './components/states/States';
+import Header from "./components/header/Header";
+
+ReactDOM.render(
+    <Header/>,
+    document.getElementById('reactappHeader')
+);
+
+ReactDOM.render(
+  <States />,
+  document.getElementById('reactapp'),
+);
